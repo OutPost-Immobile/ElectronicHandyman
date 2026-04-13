741e3399253a2e090c524d4b9f4634de78677725
Filip Kwiek <filipkwiek888@gmail.com>
2026-04-13 21:31:39 +0200

    cleanup

--- ElectronicHandyman.Api/Program.cs
+++ ElectronicHandyman.Api/Program.cs
@@ -40,20 +40,6 @@ app.UseHttpsRedirection();
 
 app.MapScrapping();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.MapPost("/api/image/upload", async (IFormFile file) =>
     {
 
